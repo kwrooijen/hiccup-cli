aec1cb3e4461c72dd58feb91898b8734b20faa1a
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-01-23 11:25:28 +0100

    Add extra paste functions

--- elisp/hiccup-cli.el
+++ elisp/hiccup-cli.el
@@ -10,14 +10,34 @@
 	(kill-new clipboard-text))
     (car kill-ring)))
 
-(defun hiccup-cli--paste-as-hiccup ()
-  "Paste the HTML in your clipboard as Hiccup syntax."
-  (interactive)
-  (write-region (hiccup-cli--clipboard-string) nil "~/tmp-hiccup-cli")
+(defun hiccup-cli--insert ()
+  "docstring"
   (save-excursion
     (insert
      (shell-command-to-string
-      "hiccup-cli --html-file ~/tmp-hiccup-cli"))))
+      "hiccup-cli --html-file /tmp/hiccup-cli"))))
+
+(defun hiccup-cli--paste-as-hiccup ()
+  "Paste the HTML in your clipboard as Hiccup syntax."
+  (interactive)
+  (write-region (hiccup-cli--clipboard-string) nil "/tmp/hiccup-cli")
+  (hiccup-cli--insert))
+
+(defun hiccup-cli--region-as-hiccup (start end)
+  "Replace the HTML in your selected region with Hiccup syntax."
+  (interactive "r")
+  (if (use-region-p)
+      (let ((region-str (buffer-substring start end)))
+        (kill-region start end)
+        (write-region region-str nil "/tmp/hiccup-cli")
+        (hiccup-cli--insert))))
+
+
+(defun hiccup-cli--yank-as-hiccup (start end)
+  "Paste the HTML in your kill-ring as Hiccup syntax."
+  (interactive "r")
+  (write-region (substring-no-properties (car kill-ring)) nil "/tmp/hiccup-cli")
+  (hiccup-cli--insert))
 
 (provide 'hiccup-cli)
 
